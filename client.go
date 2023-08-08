6d74ccd2cc720c38cf9328f4ab5ab1d0bb7856b3
James Holdren <jamesdholdren@gmail.com>
2023-08-08 10:49:44 -0400

    class: swich parent ref to struct from string (#5)

--- client.go
+++ client.go
@@ -7,18 +7,18 @@ Package quickbooks provides access to Intuit's QuickBooks Online API.
 NOTE: This library is very incomplete. I just implemented the minimum for my
 use case. Pull requests welcome :)
 
- // Do this after you go through the normal OAuth process.
- var client = oauth2.NewClient(ctx, tokenSource)
+	 // Do this after you go through the normal OAuth process.
+	 var client = oauth2.NewClient(ctx, tokenSource)
 
- // Initialize the client handle.
- var qb = quickbooks.Client{
-	 Client: client,
-	 Endpoint: quickbooks.SandboxEndpoint,
-	 RealmID: "some company account ID"'
- }
+	 // Initialize the client handle.
+	 var qb = quickbooks.Client{
+		 Client: client,
+		 Endpoint: quickbooks.SandboxEndpoint,
+		 RealmID: "some company account ID"'
+	 }
 
- // Make a request!
- var companyInfo, err = qb.FetchCompanyInfo()
+	 // Make a request!
+	 var companyInfo, err = qb.FetchCompanyInfo()
 */
 package quickbooks
 
